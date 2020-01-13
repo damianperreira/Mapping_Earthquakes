b289dcbfb9939f5d359fbe24d30bcb51242ee821
Damian Perreira <damianperreira@gmail.com>
2020-01-13 10:36:04 -0800

    adding earthquake data for the last week

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -1,23 +1,12 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-
-// Create the map object with center and zoom level.
-// let map = L.map('mapid').setView([30, 30], 2);
-// Create the map object with a center and zoom level.
-//let map = L.map("mapid", {
-//  center: [40.7, -94.5],
-//  zoom: 4
-//});
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
 });
-// Then we add our 'graymap' tile layer to the map.
-//streets.addTo(map);
 
 // We create the dark view tile layer that will be an option for our map.
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -34,22 +23,37 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-	center: [43.7, -79.3],
-zoom: 11,
+	center: [39.5, -98.5],
+	zoom: 3,
 	layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL
-let torontoHoods = 'https://raw.githubusercontent.com/damianperreira/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json';
-
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-    console.log(data);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data).addTo(map);
 });
+// Accessing the toronto GeoJSON URL
+//let torontoHoods = 'https://raw.githubusercontent.com/damianperreira/Mapping_Earthquakes/Earthquakes_past7days/torontoNeighborhoods.json';
 
+// Grabbing our GeoJSON data.
+//d3.json(torontoHoods).then(function(data) {
+//    console.log(data);
+//  // Creating a GeoJSON layer with the retrieved data.
+//  L.geoJson(data).addTo(map);
+//});
+
+
+// Create the map object with center and zoom level.
+// let map = L.map('mapid').setView([30, 30], 2);
+// Create the map object with a center and zoom level.
+//let map = L.map("mapid", {
+//  center: [40.7, -94.5],
+//  zoom: 4
+//});
+
+//notes:
 //https://raw.githubusercontent.com/damianperreira/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoRoutes.json

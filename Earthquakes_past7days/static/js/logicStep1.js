1480ab46c35285a0fade3fefbb4ec8d25b935227
Damian Perreira <damianperreira@gmail.com>
2020-01-13 21:26:43 -0800

    format circles

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -1,5 +1,5 @@
 // Add console.log to check to see if our code is working.
-console.log("working");
+console.log("step 1 is working");
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -30,30 +30,3 @@ let map = L.map('mapid', {
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
-
-// Retrieve the earthquake GeoJSON data.
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
-});
-// Accessing the toronto GeoJSON URL
-//let torontoHoods = 'https://raw.githubusercontent.com/damianperreira/Mapping_Earthquakes/Earthquakes_past7days/torontoNeighborhoods.json';
-
-// Grabbing our GeoJSON data.
-//d3.json(torontoHoods).then(function(data) {
-//    console.log(data);
-//  // Creating a GeoJSON layer with the retrieved data.
-//  L.geoJson(data).addTo(map);
-//});
-
-
-// Create the map object with center and zoom level.
-// let map = L.map('mapid').setView([30, 30], 2);
-// Create the map object with a center and zoom level.
-//let map = L.map("mapid", {
-//  center: [40.7, -94.5],
-//  zoom: 4
-//});
-
-//notes:
-//https://raw.githubusercontent.com/damianperreira/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoRoutes.json

9305fd4228487d34830e90253f258ec4d2f0ec98
Damian Perreira <damianperreira@gmail.com>
2020-01-14 10:12:24 -0800

    earthquake layer

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -51,8 +51,8 @@ function getColor(magnitude) {
 	  },
 		// We set the style for each circleMarker using our styleInfo function.
 		style: styleInfo,
-		//
-		//
+		// We create a popup for each circleMarker to display the magnitude and location of the earthquake
+		// after the marker has been created and styled.
 		onEachFeature: function(feature, layer) {
 		layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
 		}	  

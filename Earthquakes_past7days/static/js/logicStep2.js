07fbef2d009935f30b7519d0d5037a20446f166c
Damian Perreira <damianperreira@gmail.com>
2020-01-14 11:04:21 -0800

    add legend to map

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -24,16 +24,9 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 		return magnitude * 4;
 			}
 
-  // Creating a GeoJSON layer with the retrieved data.
-  //L.geoJson(data, {
-  //  pointToLayer: function(feature, latlng) {
-//		  console.log(data);
-//		  return L.circleMarker(latlng);
+
 	  });
-		// We set the style for each circleMarker using our styleInfo function.
-//		style: styleInfo	  
-//  }).addTo(map);
-//});
+
 
 
 
